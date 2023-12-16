085bfaa4a4689bc787c6ba4c2b0fb8362894bce8
Robert Bates <thegreatbobini@gmail.com>
2023-12-16 09:51:26 -0800

    consolidating repos

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "StateTesting",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+      ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -13,12 +17,19 @@ let package = Package(
     ],
     dependencies: [
         //this is where you would put any dependencies
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "StateTesting"),
+            name: "StateTesting",
+            dependencies: [
+                .product(name: "CustomDump", package: "swift-custom-dump"),
+                .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
+            ]
+        ),
         .testTarget(
             name: "StateTestingTests",
             dependencies: ["StateTesting"]),

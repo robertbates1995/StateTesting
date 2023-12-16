b8c3daf6f2f81a2eeb7fcb8fa5524f3ee07549d8
Robert Bates <thegreatbobini@gmail.com>
2023-12-16 09:55:33 -0800

    adding test application to package

---
+++ StateTestingExample/StateTestingExample/StateTestingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateTestingExampleApp.swift
+//  StateTestingExample
+//
+//  Created by Robert Bates on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct StateTestingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
